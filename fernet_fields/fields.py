e171fe0c87cad7f499c45c70d5eae06e39b4b342
Brian Werth <brian.werth@salespad.net>
2023-10-04 08:18:17 -0400

    chore: migrate package to django 4.x, configure upload to Cavallo packages [CAV-4352] (#1)
    chore: migrate package to django 4.x, configure upload to Cavallo packages [CAV-4352]
    
    ## Why
    
    - Needed to fork an otherwise abandoned package
    
    ## How
    
    - Took the forked version and upgraded its Django references
    - Added a .pypirc and github action to publish to our sources
    - Cleaned up odds and ends in the existing repo
    - Migrated to a pyproject.toml
    
    
    [CAV-4352]: https://cavallo.atlassian.net/browse/CAV-4352?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    Authored by Daniel Smith and Luke Dobben

--- fernet_fields/fields.py
+++ fernet_fields/fields.py
@@ -2,71 +2,61 @@ from cryptography.fernet import Fernet, MultiFernet
 from django.conf import settings
 from django.core.exceptions import FieldError, ImproperlyConfigured
 from django.db import models
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes, force_str
 from django.utils.functional import cached_property
 
 from . import hkdf
 
 
 __all__ = [
-    'EncryptedField',
-    'EncryptedTextField',
-    'EncryptedCharField',
-    'EncryptedEmailField',
-    'EncryptedIntegerField',
-    'EncryptedDateField',
-    'EncryptedDateTimeField',
+    "EncryptedField",
+    "EncryptedTextField",
+    "EncryptedCharField",
+    "EncryptedEmailField",
+    "EncryptedIntegerField",
+    "EncryptedDateField",
+    "EncryptedDateTimeField",
 ]
 
 
 class EncryptedField(models.Field):
     """A field that encrypts values using Fernet symmetric encryption."""
-    _internal_type = 'BinaryField'
+
+    _internal_type = "BinaryField"
 
     def __init__(self, *args, **kwargs):
-        if kwargs.get('primary_key'):
-            raise ImproperlyConfigured(
-                "%s does not support primary_key=True."
-                % self.__class__.__name__
-            )
-        if kwargs.get('unique'):
-            raise ImproperlyConfigured(
-                "%s does not support unique=True."
-                % self.__class__.__name__
-            )
-        if kwargs.get('db_index'):
-            raise ImproperlyConfigured(
-                "%s does not support db_index=True."
-                % self.__class__.__name__
-            )
+        if kwargs.get("primary_key"):
+            raise ImproperlyConfigured("%s does not support primary_key=True." % self.__class__.__name__)
+        if kwargs.get("unique"):
+            raise ImproperlyConfigured("%s does not support unique=True." % self.__class__.__name__)
+        if kwargs.get("db_index"):
+            raise ImproperlyConfigured("%s does not support db_index=True." % self.__class__.__name__)
         super(EncryptedField, self).__init__(*args, **kwargs)
 
     @cached_property
     def keys(self):
-        keys = getattr(settings, 'FERNET_KEYS', None)
+        keys = getattr(settings, "FERNET_KEYS", None)
         if keys is None:
             keys = [settings.SECRET_KEY]
         return keys
 
     @cached_property
     def fernet_keys(self):
-        if getattr(settings, 'FERNET_USE_HKDF', True):
-            return [hkdf.derive_fernet_key(k) for k in self.keys]
+        if getattr(settings, "FERNET_USE_HKDF", True):
+            return [hkdf.derive_fernet_key(k) for k in self.keys]  # type: ignore
         return self.keys
 
     @cached_property
     def fernet(self):
-        if len(self.fernet_keys) == 1:
-            return Fernet(self.fernet_keys[0])
-        return MultiFernet([Fernet(k) for k in self.fernet_keys])
+        if len(self.fernet_keys) == 1:  # type: ignore
+            return Fernet(self.fernet_keys[0])  # type: ignore
+        return MultiFernet([Fernet(k) for k in self.fernet_keys])  # type: ignore
 
     def get_internal_type(self):
         return self._internal_type
 
     def get_db_prep_save(self, value, connection):
-        value = super(
-            EncryptedField, self
-        ).get_db_prep_save(value, connection)
+        value = super(EncryptedField, self).get_db_prep_save(value, connection)  # type: ignore
         if value is not None:
             retval = self.fernet.encrypt(force_bytes(value))
             return connection.Database.Binary(retval)
@@ -74,35 +64,30 @@ class EncryptedField(models.Field):
     def from_db_value(self, value, expression, connection, *args):
         if value is not None:
             value = bytes(value)
-            return self.to_python(force_text(self.fernet.decrypt(value)))
+            return self.to_python(force_str(self.fernet.decrypt(value)))
 
     @cached_property
     def validators(self):
         # Temporarily pretend to be whatever type of field we're masquerading
         # as, for purposes of constructing validators (needed for
         # IntegerField and subclasses).
-        self.__dict__['_internal_type'] = super(
-            EncryptedField, self
-        ).get_internal_type()
+        self.__dict__["_internal_type"] = super(EncryptedField, self).get_internal_type()
         try:
             return super(EncryptedField, self).validators
         finally:
-            del self.__dict__['_internal_type']
+            del self.__dict__["_internal_type"]
 
 
 def get_prep_lookup(self):
     """Raise errors for unsupported lookups"""
-    raise FieldError("{} '{}' does not support lookups".format(
-        self.lhs.field.__class__.__name__, self.lookup_name))
+    raise FieldError("{} '{}' does not support lookups".format(self.lhs.field.__class__.__name__, self.lookup_name))
 
 
 # Register all field lookups (except 'isnull') to our handler
-for name, lookup in models.Field.class_lookups.items():
+for name, lookup in models.Field.class_lookups.items():  # type: ignore
     # Dynamically create classes that inherit from the right lookups
-    if name != 'isnull':
-        lookup_class = type('EncryptedField' + name, (lookup,), {
-            'get_prep_lookup': get_prep_lookup
-        })
+    if name != "isnull":
+        lookup_class = type("EncryptedField" + name, (lookup,), {"get_prep_lookup": get_prep_lookup})
         EncryptedField.register_lookup(lookup_class)
 
 
